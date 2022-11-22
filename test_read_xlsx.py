4b0c8e1aa5d2fb3e3ef85688c769eee922ef73ed
Stephen Gilmore <stephen@gilmore.us>
2022-11-22 11:45:25 -0800

    test read xlsx

--- test_read_xlsx.py
+++ test_read_xlsx.py
@@ -5,13 +5,13 @@ from compare_reports import ReportCompare
 
 @pytest.fixture
 def same_xlsx():
-    same_csv_1 = Path(__file__).resolve().parent / "test_files" / "same_xlsx.xlsx"
-    same_csv_2 = Path(__file__).resolve().parent / "test_files" / "same_xlsx.xlsx"
+    same_xlsx_1 = Path(__file__).resolve().parent / "test_files" / "same_xlsx.xlsx"
+    same_xlsx_2 = Path(__file__).resolve().parent / "test_files" / "same_xlsx.xlsx"
 
     return ReportCompare(
-        file1=same_csv_1,
+        file1=same_xlsx_1,
         file1_name="file_1.xlsx",
-        file2=same_csv_2,
+        file2=same_xlsx_2,
         file2_name="file_2.xlsx",
         key="eid",
     )
@@ -26,6 +26,17 @@ def test_xlsx_df_shape(same_xlsx):
     # test
     assert rc.df1.shape == (3, 4)
     assert rc.df1.shape == rc.df2.shape
+
+
+def test_xlsx_df_keys(same_xlsx):
+    """Test the shape of the dataframes to ensure they imported everything"""
+
+    # set up test case
+    rc = same_xlsx
+
+    # etst
+    assert rc.df1.index.name == "eid"
+    assert rc.df2.index.name == "eid"
 
 
 def test_compare_xlsx_summary(same_xlsx):
